b15a3256a81f26cd0784851f656f6182649261d9
Tsunami <tsunamidanaem@gmail.com>
2024-11-20 18:14:23 -0600

    Mejora de validacion de fecha

--- reto1.py
+++ reto1.py
@@ -1,32 +1,41 @@
 """
-Desarrollo de una aplicacion que envie un mensaje recordatorio que 
-tienen libros pendientes por devolver
+Desarrollo de una aplicacion que envie un mensaje recordatorio 
+para devolver un libro
 
-1. Solicitar al usuario que ingrese:
-Nombre, titulo del libre que tiene pendiente, fecha vencimiento
-2. Crear la funcion generar_recordatorio
+Esta funcion solicita los datos del usuario, valida el formato de la fecha
+y crea un mensaje recordatorio personalizado.
+
+Args:
+    nombre (str)
+    titulo_dellibro (str)
+    fecha_de_vencimiento (str)
+Returns:
+    str: mensaje de recordatorio
+Raises: 
+    ValueError: si la fecha no esta en el formato
 """
 
+from datetime import datetime
 
-def generar_recordatorio():
 
+def generar_recordatorio(nombre, titulo_del_libro, fecha_de_vencimiento):
+    try:
+        fecha = datetime.strptime(fecha_de_vencimiento, "%d/%m/%Y")
+        mensaje_fecha = f", antes del {fecha.day} de {fecha.month} de {fecha.year}"
+    except ValueError as e:
+        print(f"Error: {e}, introduzca una fecha valida de modo (DD/MM/AAAA)")
+        return None
     mensaje = (
-        "Hola {0}, recuerda devolver el libro '{1}' antes del {2} de {3} de {4}".format(
-            nombre,
-            titulo_del_libro,
-            fecha_de_venciemiento[0:2],
-            fecha_de_venciemiento[3:5],
-            fecha_de_venciemiento[6:10],
-        )
+        f"Hola {nombre}, recuerda devolver el libro '{titulo_del_libro}'{mensaje_fecha}"
     )
     return mensaje
 
 
 nombre = input("¿Cual es tu nombre: ")
 titulo_del_libro = input("A continuacion, indique el titulo del libro: ")
-fecha_de_venciemiento = input(
+fecha_de_vencimiento = input(
     "Finalmente, indique la fecha de vencimiento (DD/MM/AAAA): "
 )
 # Llamando a la funcion generar_recordatorio
-recordatorio = generar_recordatorio()
+recordatorio = generar_recordatorio(nombre, titulo_del_libro, fecha_de_vencimiento)
 print(recordatorio)
